d4a0cdc99ce9c631aa877a01ac583f81716d04cf
jakehirzel <hirzel@hotmail.com>
2016-05-07 12:49:48 -0400

    Added jQuery to append .odd to first article in any row with odd number of articles.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+// Custom jQuery
 
+// Apply .odd to first article in any rows with odd article amounts
+var main = function() {
+  $('.row').each(function() {
+    if ($(this).children('article').length % 2 != 0) {
+      $(this).children('article:first').addClass('odd');
+    }
+  });
+};
+
+// Run main
+$(document).ready(main);
